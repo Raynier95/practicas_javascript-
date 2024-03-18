6f12227bc73e765a343e0056313afa79b3d2d0ad
Raynier <manfiresssj@gmail.com>
2024-03-18 13:16:58 -0400

    añadiendo sintaxis y operadores

---
+++ sintaxis_js/operadores.js
@@ -0,0 +1,23 @@
+// En JS hay varios tipos de operadores entre los cuales tenemos:
+// Matematicos, Asignacion e Incremento/Decremento
+
+// Matematicos
+// suma / concatenar -> +
+// resta -> - 
+// multiplicacion -> *
+// division -> /
+//  modulo -> % 
+
+// Asignacion
+// Asignacion -> =
+//  suma -> a+=3 -> (a = a + 3)
+//  resta -> a-=3 -> (a = a - 3)
+// mult -> a*=3 -> (a = a * 3)
+// div -> a/=3 -> (a = a / 3)
+// mod -> a%=3 -> (a = a % 3)
+
+// Incremento / Decremento
+// post-incremento en 1 = a++
+// pre-incremento en 1 = ++a
+// post-decremento en 1 = a--
+// pre-decremento en 1 = --a
---
+++ sintaxis_js/sintaxis.js
@@ -0,0 +1,22 @@
+// JS es un lenguaje de programacion que puede interpretar el navegador
+
+// Sintaxis
+// JS es case sensitive Numero no es igual a numero
+// Es de tipado dinamico o debil, las variables son del tipo del dato que almacenan
+// Las sentencias finalizan con ; (no es obligatorio, pero es recomendable)
+
+// Variables
+// Una variable es un espacio que reservamos en memoria para almacenar un dato que podra cambiar durante la ejecucion del programa. 
+// La palabra reservado para emplear es "let" (No es recomendable usar var)
+
+// let auto = 'toyota' //Ejemplo de variable en JS
+
+// Una varaible se puede declarar, inicializar y modificar
+
+// Constantes
+// Una constante es un espacio que reservamos en memoria para almacenar un dato que no cambiara durante la ejecucion del programa.
+// La palabra reservado es "const"
+
+// const nombre = 'Juan' //Ejemplo de constante en JS
+
+// El Scope o ambito es la zona donde existe nuestra variable o constante
